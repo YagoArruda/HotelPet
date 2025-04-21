04fe759076c136db5fe8e7469e5c06881950c574
YagoArruda <yago4444@gmail.com>
2025-04-21 18:58:15 -0300

    Ajuste no login

--- HotelPet/src/components/LoginBody.jsx
+++ HotelPet/src/components/LoginBody.jsx
@@ -3,12 +3,29 @@ import './LoginBody.css'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import womanAndCat from '../assets/womanAndCat.svg'
+import {useState } from 'react';
 
 function LoginBody(){
     const navigate = useNavigate()
+
+    const [login, setLogin] = useState("");
+        const handleChangeLogin = (event) => {
+            setLogin(event.target.value);
+        };
+    
+        const [senha, setSenha] = useState("");
+        const handleChangeSenha = (event) => {
+            setSenha(event.target.value);
+        };
     
     function Autenticar(){
-        navigate('/Agenda')
+
+        if(login == "admin" && senha == "admin"){
+            navigate('/Agenda')
+        }
+        else{
+            alert("Dados Incorretos!");
+        }
     }
 
     return (
@@ -19,8 +36,8 @@ function LoginBody(){
             <div className='loginCard'>
                 <h2>Login</h2>
 
-                <input type="user" className='loginInput' placeholder='Usuario'/>
-                <input type="password" className='loginInput' placeholder='Senha'/>
+                <input type="user" className='loginInput' placeholder='Usuario' onChange={handleChangeLogin}/>
+                <input type="password" className='loginInput' placeholder='Senha' onChange={handleChangeSenha}/>
 
                 <p className='loginButton' onClick={Autenticar}>
                     <Link to='/' className='noLinkStyle'>Entrar</Link>
